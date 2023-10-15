b7019fd0aca71811b6d36e8e9a567b7042d14f57
Derron Jackson <derron@Derrons-MacBook-Pro.local>
2023-10-15 13:05:25 -0500

    initial spring boot project

---
+++ src/test/java/com/example/TodoDemo/TodoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
